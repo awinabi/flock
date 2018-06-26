ac8ace67ab2ae6500d2d624befcd128912437c2c
Awin Abi <awin@awinawin.awin>
2018-06-26 17:12:03 +0530

    Reduce some packages

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -33,14 +32,12 @@ var sessionConfig = {
 };
 
 if (app.get('env') === 'production') {
-  app.set('trust proxy', 1);
+  // app.set('trust proxy', 1);
   sessionConfig.cookie.secure = true;
 }
 
 app.use(session(sessionConfig));
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
